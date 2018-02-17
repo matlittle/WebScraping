7a16b43794cf901a0ddf4702ad75e2107318dc13
matlittle <matlittle727@gmail.com>
2018-02-17 11:38:57 -0600

    Added routes and functions to save and retreive notes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -8,9 +8,17 @@ module.exports = (app) => {
     res.send(result);
   });
 
-  app.get('/api/retrieve', async (req, res) => {
-    const savedArticles = await articles.retreiveArticles();
-    
-    res.json(savedArticles);
+  app.get("/api/article/:id", function(req, res) {
+    const result = await articles.getArticle(req.params.id);
+
+    res.send(result);
   });
+
+  app.post("/api/article/:id", function(req, res) {
+    const result = await articles.saveNote(req.body, req.params.id);
+
+    res.send(result);
+  });
+
+
 }
--- controllers/articles.js
+++ controllers/articles.js
@@ -18,8 +18,32 @@ module.exports.saveArticle = async article => {
 }
 
 module.exports.retreiveArticles = async () => {
-  const articles = db.Articles.find({})
+  const articles = await db.Articles.find({})
   
   console.log(articles);
   return articles;
+}
+
+module.exports.getArticle = async (id) => {
+  const article = db.Article.findOne({_id: id}).populate('note');
+
+  console.log(article);
+  return article;
+}
+
+module.exports.saveNote = async (note, id) => {
+  const noteResult = await db.Note.create(note);
+
+  console.log(noteResult);
+
+  const articleResult = await 
+    db.Article.findOneAndUpdate(
+      {_id: id}, 
+      {note: noteResult._id}, 
+      {new: true}
+    );
+
+  console.log(articleResult);
+
+  return articleResult;
 }
